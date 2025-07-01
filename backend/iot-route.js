fbb3494d1ef88483539d7e863b6ece133b778f8a
Widhyatma Sensei <evanalifwidhyatma@gmail.com>
2025-07-01 20:21:21 +0700

    feat: add backend files for IoT-based Water Quality Monitoring System

---
+++ backend/iot-route.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql2');
+
+const app = express();
+const port = 2518;
+
+// ===== Middleware =====
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false }));
+
+// ===== Koneksi MySQL =====
+const db = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'DB_AIR'
+});
+
