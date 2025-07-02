d33b5bc005e18489e0a67dc2f1bc2d2165972670
Widhyatma Sensei <evanalifwidhyatma@gmail.com>
2025-07-02 16:26:18 +0700

    feat: implement GET endpoint for data sensor submission with validation and error handling

--- backend/iot-route.js
+++ backend/iot-route.js
@@ -17,3 +17,31 @@ const db = mysql.createPool({
   database: 'DB_AIR'
 });
 
+// API TABEL DATA_SENSOR
+// ===== ROUTE: GET kirim via URL-encoded =====
+// contoh: GET /api/data-sensor/send?id_sensor=1&temperature=24.5&humidity=60&moisture=30&light=500
+app.get('/api/data-sensor/send', (req, res) => {
+  const { id_sensor, temperature, amonia, ph} = req.query;
+
+  if ( !id_sensor || !temperature  || !amonia || !ph) {
+    return res.status(400).send('Semua parameter (id_sensor, temperature, amonia, ph) wajib diisi.');
+  }
+
+  const sql = `
+    INSERT INTO DATA_SENSOR (id_sensor, temperature, amonia, ph) 
+    VALUES (?, ?, ?, ?, ?)
+  `;
+
+  db.query(sql, [id_sensor, temperature, amonia, ph], (err) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('Gagal menyimpan data.');
+    }
+    console.log(`Data berhasil disimpan (GET): id_sensor=${id_sensor}, temperature=${temperature}, amonia=${amonia}, ph=${ph}`);
+    res.send(`Data diterima via GET: 
+      id_sensor=${id_sensor},
+      temperature=${temperature}, 
+      amonia=${amonia}, 
+      ph=${ph}`);
+  });
+});
