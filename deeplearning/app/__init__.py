6d19c07e7e2f2025a2ef9128fa875176045b94dc
Widhyatma Sensei <evanalifwidhyatma@gmail.com>
2025-07-04 17:37:48 +0700

    feat: initialize Flask app with CORS support

--- deeplearning/app/__init__.py
+++ deeplearning/app/__init__.py
@@ -1,3 +1,11 @@
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+from app import routes
+
 # Kosongkan atau hanya inisialisasi package
 # Tidak perlu import get_model/get_scaler kalau tidak digunakan
 
